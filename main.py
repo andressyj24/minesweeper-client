23f45185c6818db801d1e6918641e59eb614501c
Andres Salazar <andressyj24@gmail.com>
2021-05-14 13:14:56 -0500

    Implemented calls to minesweeper api for testing purposes

--- main.py
+++ main.py
@@ -22,19 +22,24 @@ def create_new_game(new_game_request: NewGameRequest):
     data = {'rows': new_game_request.rows, 'columns': new_game_request.columns, 'timer': new_game_request.timer,
             'difficulty': new_game_request.difficulty}
     create_game_url = f'{game_service_url}api/games'
-    return requests.post(create_game_url, data)
+    return requests.post(create_game_url, json=data)
 
 
 def get_game(game_id):
-    get_game_url = f'https://mine-sweeper-game.herokuapp.com/api/games/{game_id}'
+    get_game_url = f'{game_service_url}api/games/{game_id}'
     return requests.get(get_game_url)
 
 
 def update_cell(update_cell_request: UpdateGameRequest):
     data = {'row': update_cell_request.row, 'columns': update_cell_request.column, 'action': 'FLAG'}
-    update_game_url = f'https://mine-sweeper-game.herokuapp.com/api/games/{update_cell_request.game_id}/boards'
-    return requests.patch(update_game_url, data)
+    update_game_url = f'{game_service_url}api/games/{update_cell_request.game_id}/boards'
+    return requests.patch(update_game_url, json=data)
 
 
 if __name__ == '__main__':
-    create_new_game(NewGameRequest(5, 7, 5, 'MEDIUM'))
+    new_game_request = NewGameRequest(rows=5, columns=7, timer=5, difficulty='MEDIUM')
+    new_game_response = create_new_game(new_game_request).json()
+    game_id = new_game_response['game_id']
+
+    game_state = update_cell(UpdateGameRequest(game_id=game_id, row=0, column=0, action='open')).json()
+    print(game_state)
